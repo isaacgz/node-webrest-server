97e9cdf7f401c09473539a27c8f98541f1c7a484
Isaac Garcia <joelgarcia405@gmail.com>
2024-01-13 18:19:47 -0600

    first commit

---
+++ src/app.http.ts
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import http from 'http';
+
+const server = http.createServer((req,res) => {
+
+    console.log(req.url);
+
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write(`<h1>${req.url}</h1>`);
+    // res.end();
+
+    // const data = {
+    //     name: 'Jhon Doe',
+    //     age: 30,
+    //     city:'New york'
+    // };
+
+    // res.writeHead(200,{'Content-Type': 'application/json'});
+    // res.end(JSON.stringify(data));
+
+    if( req.url === '/'){
+        const htmlFile= fs.readFileSync('./public/index.html','utf-8');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end( htmlFile );
+        return;
+    } 
+    
+    if(req.url?.endsWith('.css')){
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end();
+    }else if(req.url?.endsWith('.js')){
+        res.writeHead(404, {'Content-Type': 'application/javascript'});
+        res.end();
+    }
+
+    const responseContent = fs.readFileSync(`./public${ req.url }`, 'utf-8');
+    res.end(responseContent);
+});
+
+
+server.listen(8080, () => {
+    console.log('Server running in port 8080');
+
+})
