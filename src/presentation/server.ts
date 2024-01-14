d9d2341a0cae647db0867dc08c7534407c83123e
Isaac Garcia <joelgarcia405@gmail.com>
2024-01-14 15:44:47 -0600

    agrega funciones de crud

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -12,26 +13,36 @@ export class Server{
 
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options){
-        const { port, public_path = 'public'} = options;
+        const { port, routes, public_path = 'public'} = options;
         this.port = port;
+        this.routes = routes;
         this.publicPath = public_path; 
     }
 
     async start() {
 
         //* Middlewares
+        this.app.use( express.json()); //raw
+        this.app.use( express.urlencoded({ extended: true })); // x-www-form-urlencoded
 
         //* Public Folder
         this.app.use( express.static(this.publicPath) );
 
+        //* Routes
+        this.app.use( this.routes );
+        
+
+        //* SPA
         this.app.get('*', (req,res) => {
             const indexPath = path.join( __dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
             return;
         });
 
+        //* Port
         this.app.listen(this.port, () => {
             console.log(`Server running in port ${this.port}`)
         });
